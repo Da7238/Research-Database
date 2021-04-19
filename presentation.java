67202e15673a0d533a0d96cc688b1619667351a7
kingcolton <clb4416@g.rit.edu>
2021-04-19 16:49:52 -0400

    Update presentation.java
    Wrote empty methods and added comments.

--- presentation.java
+++ presentation.java
@@ -3,6 +3,66 @@
 // Colton Bailiff
 // Weijie Chen
 // Ben Donahue
-public class presentation {
 
+import java.util.*;
+import java.util.Scanner;
+
+public class presentation {
+   // Use a external java file to perform backend tasks
+   researchDatabase db = new researchDatabase();
+   
+   // TO BE ASSIGNED
+   // Constructor
+   public presentation() {
+      //db.connect(); // Connect to the database
+      
+      // GUI Variables
+      // TextField
+      // TextArea
+      // Label
+      // Search bar
+      // Drop list
+   }
+   
+   // ALL GROUP MEMBERS ARE ASSIGNED TO EDIT THIS METHOD
+   // Button pressed events
+   public void handle(ActionEvent evt) {
+      // Use switch here for each buttons
+   }
+   
+   // TO BE ASSIGNED
+   // Search bar to look up for author, student, faculty, etc
+   public void searchBar() {
+   
+   }
+   
+   // TO BE ASSIGNED
+   // View a article page with title, content, and author name
+   public void articlePage() {
+   
+   }
+   
+   // TO BE ASSIGNED
+   // Edit a article
+   public void editArticlePage() {
+   
+   }
+   
+   // TO BE ASSIGNED
+   // Add a faculty
+   public void addFacultyPage() {
+   
+   }
+   
+   // TO BE ASSIGNED
+   // View author information page
+   public void authorInfoPage() {
+   
+   }
+
+   // Launch the program
+   public static void main(String[] args) {
+      new presentation();
+      System.exit(0);
+   }
 }

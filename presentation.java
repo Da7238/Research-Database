2331bb1c22151ca2376bcf76fd49433b89368fa4
kingcolton <clb4416@g.rit.edu>
2021-04-25 19:34:42 -0400

    Major overhaul on presentation.java to simplify variables and renders, as well as fixed login and switch scenes.

--- presentation.java
+++ presentation.java
@@ -26,70 +26,48 @@ public class presentation extends Application implements EventHandler<ActionEven
    private Scanner scanner = new Scanner(System.in);
 
    private Stage stage;
-   private Scene scene;
-   
-   //Student menu
-   private Stage studentstage;
-   private Scene studentScene;
-   
-   //Faculty menu
-   private Stage facultystage;
-   private Scene facultyScene;
+   private Scene scene, studentScene, facultyScene;
    
    private VBox root = new VBox(1);
-   private GridPane gridPane = new GridPane();
-   //private FlowPane flowPane = new FlowPane(8,8);
+   private VBox rootStudentMenu = new VBox(1);
+   private VBox rootFacultyMenu = new VBox(1);
+   
+   private GridPane gridPane;
+   private FlowPane flowPane;
    
    private Label titleLabel;
-   private Label emailLabel = new Label("Email:");
-   private Label passwordLabel = new Label("Password:");
+   private Label emailLabel;
+   private Label passwordLabel;
 
    private TextField emailTF = new TextField();
    private PasswordField passwordTF = new PasswordField();
    
-   private Button loginBtn = new Button("Login");
+   private Button loginBtn;
    
-   // Label, TextFile, Button for student.
-   private Label studentOptions;
-   private Label studentSearch1 = new Label("Search Article By Title");
-   private Label studentSearch2 = new Label("Search Article By Abstract");
-   private Label studentSearch3 = new Label("Search Article By Author");
-   private Label studentSearch4 = new Label("Search Faculty By Email");
-   private Label studentSearch5 = new Label("Search Faculty By Name");
-   private Label studentSearch6 = new Label("Search Faculty By Abstract");
-   private Label studentSearch7 = new Label("Search Faculty By Department");
+   // Labels, TextFields, and Buttons for student and faculty.
+   private Label labelSearch1;
+   private Label labelSearch2;
+   private Label labelSearch3;
+   private Label labelSearch4;
+   private Label labelSearch5;
+   private Label labelSearch6;
+   private Label labelSearch7;
+   private Label facultyInsert;
+   private Label facultyDeleteEntry;
+   private Label facultyUpdateTitle;
    
-   private TextField studentText1 = new TextField();
-   private TextField studentText2 = new TextField();
-   private TextField studentText3 = new TextField();
-   private TextField studentText4 = new TextField();
-   private TextField studentText5 = new TextField();
-   private TextField studentText6 = new TextField();
-   private TextField studentText7 = new TextField();
+   private TextField searchTF1;
+   private TextField searchTF2;
+   private TextField searchTF3;
+   private TextField searchTF4;
+   private TextField searchTF5;
+   private TextField searchTF6;
+   private TextField searchTF7;
 
-   
-   // Label, TextFile, Button for student.
-   private Label facultyOptions;
-   private Label facultySearch1 = new Label("Search Student By Name");
-   private Label facultySearch2 = new Label("Search Student By Major");
-   private Label facultySearch3 = new Label("Search Student By Interest");
-   private Label facultySearch4 = new Label("Search Student By Email");
-   private Label facultyInsert = new Label("Insert Faculty");
-   private Label facultyDeleteEntry = new Label("Delete Article By Title");
-   private Label facultyUpdateTitle = new Label("Update Article Title");
-   
-   private TextField facultyText1 = new TextField();
-   private TextField facultyText2 = new TextField();
-   private TextField facultyText3 = new TextField();
-   private TextField facultyText4 = new TextField();
-   private TextField facultyText5 = new TextField();
-   private TextField facultyText6 = new TextField();
-   private TextField facultyText7 = new TextField();
-
-   private Button searchBtn = new Button("Search");
-   private Button insertBtn = new Button("Insert");
-   private Button deleteBtn = new Button("delete");
-   private Button updateBtn = new Button("update");
+   private Button searchBtn;
+   private Button insertBtn;
+   private Button deleteBtn;
+   private Button updateBtn;
    
    // Constructor
    public void start(Stage _stage) throws Exception {
@@ -102,7 +80,17 @@ public class presentation extends Application implements EventHandler<ActionEven
       titleLabel = new Label("Login");
       titleLabel.setMinHeight(80);
       titleLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 30px;");
-      // 2
+      
+      // Label settings
+      emailLabel = new Label("Email:");
+      passwordLabel = new Label("Password:");
+      
+      // Button label
+      loginBtn = new Button("Login");
+      
+      // Create a new grid
+      gridPane = new GridPane();
+
       // Re-align the Label text to the center right.      
       gridPane.setHalignment(emailLabel, HPos.RIGHT);
       gridPane.setHalignment(passwordLabel, HPos.RIGHT);
@@ -127,118 +115,164 @@ public class presentation extends Application implements EventHandler<ActionEven
       stage.show();
    }
    
-   
-   public void student(Stage _studentstage) throws Exception {
-      db.connect(); // Connect to the database before render window
-      
-      stage = _studentstage;
-      stage.setTitle("Research Database");
-      
+   // Student Main Menu
+   public void studentMenu() {
       // Title label settings
-      titleLabel = new Label("student optin menu");
-   
+      titleLabel = new Label("Student Main Menu");
       titleLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 30px;");
       
+      // Search label settings
+      labelSearch1 = new Label("Search Article By Title");
+      labelSearch2 = new Label("Search Article By Abstract");
+      labelSearch3 = new Label("Search Article By Author");
+      labelSearch4 = new Label("Search Faculty By Email");
+      labelSearch5 = new Label("Search Faculty By Name");
+      labelSearch6 = new Label("Search Faculty By Abstract");
+      labelSearch7 = new Label("Search Faculty By Department");
+      
+      // Textfields
+      searchTF1 = new TextField();
+      searchTF2 = new TextField();
+      searchTF3 = new TextField();
+      searchTF4 = new TextField();
+      searchTF5 = new TextField();
+      searchTF6 = new TextField();
+      searchTF7 = new TextField();
+      
+      // Button label
+      searchBtn = new Button("Search");
+      
+      // Create a new grid
+      gridPane = new GridPane();
+      
       // Re-align the Label text to the center right.      
-      gridPane.setHalignment(studentSearch1, HPos.LEFT);
-      gridPane.setHalignment(studentSearch2, HPos.RIGHT);
-      gridPane.setHalignment(studentSearch3, HPos.LEFT);
-      gridPane.setHalignment(studentSearch4, HPos.RIGHT);
-      gridPane.setHalignment(studentSearch5, HPos.LEFT);
-      gridPane.setHalignment(studentSearch6, HPos.RIGHT);
-      gridPane.setHalignment(studentSearch7, HPos.LEFT);
+      gridPane.setHalignment(labelSearch1, HPos.LEFT);
+      gridPane.setHalignment(labelSearch2, HPos.RIGHT);
+      gridPane.setHalignment(labelSearch3, HPos.LEFT);
+      gridPane.setHalignment(labelSearch4, HPos.RIGHT);
+      gridPane.setHalignment(labelSearch5, HPos.LEFT);
+      gridPane.setHalignment(labelSearch6, HPos.RIGHT);
+      gridPane.setHalignment(labelSearch7, HPos.LEFT);
       
       // Render a grid panel
-      gridPane.addRow(0, studentSearch1, studentText1);
-      gridPane.addRow(1, studentSearch2, studentText2);
-      gridPane.addRow(2, studentSearch3, studentText3);
-      gridPane.addRow(3, studentSearch4, studentText4);
-      gridPane.addRow(4, studentSearch5, studentText5);
-      gridPane.addRow(5, studentSearch6, studentText6);
-      gridPane.addRow(6, studentSearch7, studentText7);
+      gridPane.addRow(0, labelSearch1, searchTF1);
+      gridPane.addRow(1, labelSearch2, searchTF2);
+      gridPane.addRow(2, labelSearch3, searchTF3);
+      gridPane.addRow(3, labelSearch4, searchTF4);
+      gridPane.addRow(4, labelSearch5, searchTF5);
+      gridPane.addRow(5, labelSearch6, searchTF6);
+      gridPane.addRow(6, labelSearch7, searchTF7);
       
-      root.getChildren().addAll(studentOptions, gridPane, searchBtn);
+      rootStudentMenu.getChildren().addAll(titleLabel, gridPane, searchBtn);
       
       // Finally, put everything at center
-      root.setAlignment(Pos.CENTER);
+      rootStudentMenu.setAlignment(Pos.CENTER);
       gridPane.setAlignment(Pos.CENTER);
       
-      // Render window
-      scene = new Scene(root, 500, 350);
-      stage.setScene(scene);
+      // Render a new window page
+      studentScene = new Scene(rootStudentMenu, 500, 350);
+      stage.setScene(studentScene);
       
       // Add button behaviors
       searchBtn.setOnAction(this);
-      
-      stage.show();
    }
    
-   public void faculty(Stage _facultystage) throws Exception {
-      db.connect(); // Connect to the database before render window
-      
-      stage = _facultystage;
-      stage.setTitle("Research Database");
-      
+   // Faculty Main Menu
+   public void facultyMenu() {
       // Title label settings
-      titleLabel = new Label("Faculty Option menu");
-   
+      titleLabel = new Label("Faculty Main Menu");
       titleLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 30px;");
       
+      // Search label settings
+      labelSearch1 = new Label("Search Student By Name");
+      labelSearch2 = new Label("Search Student By Major");
+      labelSearch3 = new Label("Search Student By Interest");
+      labelSearch4 = new Label("Search Student By Email");
+      facultyInsert = new Label("Insert Faculty");
+      facultyDeleteEntry = new Label("Delete Article By Title");
+      facultyUpdateTitle = new Label("Update Article Title");
+      
+      // Textfields
+      searchTF1 = new TextField();
+      searchTF2 = new TextField();
+      searchTF3 = new TextField();
+      searchTF4 = new TextField();
+      searchTF5 = new TextField();
+      searchTF6 = new TextField();
+      searchTF7 = new TextField();
+      
+      // Button label
+      searchBtn = new Button("Search Student");
+      insertBtn = new Button("Insert");
+      deleteBtn = new Button("Delete");
+      updateBtn = new Button("Update");
+      
+      // Create a new grid and flow
+      gridPane = new GridPane();
+      flowPane = new FlowPane(8,8);
+      
       // Re-align the Label text to the center right.      
-      gridPane.setHalignment(facultySearch1, HPos.LEFT);
-      gridPane.setHalignment(facultySearch2, HPos.RIGHT);
-      gridPane.setHalignment(facultySearch3, HPos.LEFT);
-      gridPane.setHalignment(facultySearch4, HPos.RIGHT);
+      gridPane.setHalignment(labelSearch1, HPos.LEFT);
+      gridPane.setHalignment(labelSearch2, HPos.RIGHT);
+      gridPane.setHalignment(labelSearch3, HPos.LEFT);
+      gridPane.setHalignment(labelSearch4, HPos.RIGHT);
       gridPane.setHalignment(facultyInsert, HPos.LEFT);
       gridPane.setHalignment(facultyDeleteEntry, HPos.RIGHT);
       gridPane.setHalignment(facultyUpdateTitle, HPos.LEFT);
       
       // Render a grid panel
-      gridPane.addRow(0, facultySearch1, facultyText1);
-      gridPane.addRow(1, facultySearch2, facultyText2);
-      gridPane.addRow(2, facultySearch3, facultyText3);
-      gridPane.addRow(3, facultySearch4, facultyText4);
-      gridPane.addRow(4, facultyInsert, facultyText5);
-      gridPane.addRow(5, facultyDeleteEntry, facultyText6);
-      gridPane.addRow(6, facultyUpdateTitle, facultyText7);
+      gridPane.addRow(0, labelSearch1, searchTF1);
+      gridPane.addRow(1, labelSearch2, searchTF2);
+      gridPane.addRow(2, labelSearch3, searchTF3);
+      gridPane.addRow(3, labelSearch4, searchTF4);
+      gridPane.addRow(4, facultyInsert, searchTF5);
+      gridPane.addRow(5, facultyDeleteEntry, searchTF6);
+      gridPane.addRow(6, facultyUpdateTitle, searchTF7);
       
-      root.getChildren().addAll(facultyOptions, gridPane, searchBtn);
-      root.getChildren().addAll(facultyOptions, gridPane, insertBtn);
-      root.getChildren().addAll(facultyOptions, gridPane, deleteBtn);
-      root.getChildren().addAll(facultyOptions, gridPane, deleteBtn);
+      flowPane.getChildren().addAll(searchBtn, insertBtn, deleteBtn);
+      rootFacultyMenu.getChildren().addAll(titleLabel, gridPane, flowPane);
       
       // Finally, put everything at center
-      root.setAlignment(Pos.CENTER);
+      rootFacultyMenu.setAlignment(Pos.CENTER);
       gridPane.setAlignment(Pos.CENTER);
+      flowPane.setAlignment(Pos.CENTER);
       
-      // Render window
-      scene = new Scene(root, 500, 350);
-      stage.setScene(scene);
+      // Render a new window page
+      facultyScene = new Scene(rootFacultyMenu, 500, 350);
+      stage.setScene(facultyScene);
       
       // Add button behaviors
       searchBtn.setOnAction(this);
       insertBtn.setOnAction(this);
       updateBtn.setOnAction(this);
       deleteBtn.setOnAction(this);
-      
-      stage.show();
    }
    
    
-
    // Button pressed events
    public void handle(ActionEvent evt) {
       Button btn = (Button)evt.getSource();
       
-      // Login button (Used alert for testing purpose)
+      // Switch to handle button actions and execute a method
       switch(btn.getText()) {
          case "Login":
             login();
             break;
-         case "Student":
-            student();
+         case "Search":
+            studentSearch();
             break;
-         case "Faculty":
+         case "Search Student":
+            facultySearch();
+            break;
+         case "Insert":
+            facultySearch();
+            break;
+         case "Delete":
+            facultySearch();
+            break;
+         case "Update":
+            facultySearch();
+            break;
       }
    }
    
@@ -247,45 +281,46 @@ public class presentation extends Application implements EventHandler<ActionEven
       String email = emailTF.getText();
       String password = passwordTF.getText();
       
-      // Using student login for now.
-      int loginInfo = db.login_student(email, password);
-      
-      // Testing purpose, might keep this.
-      if (loginInfo == 1) {
-         alert = new Alert(AlertType.INFORMATION, "Logged in!");
-         alert.showAndWait();
+      // Check if there is a existing login information before accessing to the database GUI
+      // Student login
+      if (db.login_student(email, password) == 1 && db.login_faculty(email, password) == 0) {
+         studentMenu();
       }
+      // Faculty login
+      else if (db.login_student(email, password) == 0 && db.login_faculty(email, password) == 1) {
+         facultyMenu();
+      }
+      // If not exist, display a dialog message
       else {
-         alert = new Alert(AlertType.INFORMATION, "Login error");
+         alert = new Alert(AlertType.WARNING, "LOGIN ERROR");
          alert.showAndWait();
       }
    }
    
-   // student menu
-   public void student() {
-   String studentSearch1 = studentText1.getText();
-   String studentSearch2 = studentText2.getText();
-   String studentSearch3 = studentText3.getText();
-   String studentSearch4 = studentText4.getText();
-   String studentSearch5 = studentText5.getText();
-   String studentSearch6 = studentText6.getText();
-   String studentSearch7 = studentText7.getText();
+   // Student Search functionality
+   public void studentSearch() {
+   String studentSearch1 = searchTF1.getText();
+   String studentSearch2 = searchTF2.getText();
+   String studentSearch3 = searchTF3.getText();
+   String studentSearch4 = searchTF4.getText();
+   String studentSearch5 = searchTF5.getText();
+   String studentSearch6 = searchTF6.getText();
+   String studentSearch7 = searchTF7.getText();
    
-   String searchArticle = db.searchArticle(studentSearch1);
-   
+   //String searchArticle = db.searchArticle(studentSearch1);
    }
    
-   // faculty menu
-   public void faculty() {
-   String facultySearch1 = facutlyText1.getText();
-   String facultySearch2 = facutlyText2.getText();
-   String facultySearch3 = facutlyText3.getText();
-   String facultySearch4 = facutlyText4.getText();
-   String facultyInsert = facutlyText5.getText();
-   String facultyDeleteEntry = facutlyText6.getText();
-   String facultyUpdateTitle = facutlyText7.getText();
+   // Faculty Search functionality
+   public void facultySearch() {
+   String facultySearch1 = searchTF1.getText();
+   String facultySearch2 = searchTF2.getText();
+   String facultySearch3 = searchTF3.getText();
+   String facultySearch4 = searchTF4.getText();
+   String facultyInsert = searchTF5.getText();
+   String facultyDeleteEntry = searchTF6.getText();
+   String facultyUpdateTitle = searchTF7.getText();
    
-   String facultyDeleteEntry = db.deleteEntry(facultyText6);
+   //String facultyDeleteEntry = db.deleteEntry(facultyText6);
    }
    
    // Launch the program
@@ -300,31 +335,6 @@ public class presentation extends Application implements EventHandler<ActionEven
 // -------------------------------------------
 
 // -------------------------------------------
-
-   // TO BE ASSIGNED
-   // Main menu for student after login
-   public void studentMenu() {
-   System.exit(0);
-      // panel.setLayout(new GridLayout(2, 1));
-//       student = new JButton("Student");
-// 
-//       student.addActionListener(new ActionListener() {
-// 
-//          @Override
-//          public void actionPerformed(ActionEvent e) {
-//             researchDatabase.student_menu();
-// 
-//          }
-// 
-//       });
-
-   }
-
-   // TO BE ASSIGNED
-   // Main menu for faculty after login
-   public void facultyMenu() {
-System.exit(0);
-   }
 
    // TO BE ASSIGNED
    // Search bar to look up for author, student, faculty, etc
